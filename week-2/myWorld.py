ad868d592ffd50cab7e68ee8d70a279754a8a88a
desertcoqui <fdetres@my365.bellevue.edu>
2022-08-21 18:12:27 -0500

    python

---
+++ week-2/myWorld.py
@@ -0,0 +1,9 @@
+#  Title: Assignment 2.3
+#  Author: Professor Krasso
+#  Date: 21 Aug 20222
+#  Modified By: Ferdinand "Papo" Detres Jr
+#  Description: Installation of Python3
+#  Instructors provided by Prof. Krasso's video
+
+
+print("You are now in Detres World")
