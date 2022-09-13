748ff14b4fb7407dedb1f87b295aadedbb486496
desertcoqui <fdetres@my365.bellevue.edu>
2022-09-13 00:12:05 -0500

    exit code added

--- pythonPlayground/whileLoops.py
+++ pythonPlayground/whileLoops.py
@@ -1,13 +1,16 @@
 
 # only prints to 51 because once last line of code is executed and reiterates to line four, there is no print statement after line six to continue the code. So the code breaks at this point.
-iterations = 200
-while iterations > 50:
-    print(iterations)
-    iterations = iterations - 1
-
-
-iterations = 200
-while iterations > 50:
-    print(iterations)
-    iterations = iterations - 1
+iterations = 65
 print(iterations)
+while iterations > 50:
+    print(iterations)
+    iterations = iterations - 1
+print()
+
+
+# prints to 50 because the variable is calling to line 16 after it subtracts 1
+iterations_two = 65
+while iterations_two > 50:
+    print(iterations_two)
+    iterations_two = iterations_two - 1
+print(iterations_two)
