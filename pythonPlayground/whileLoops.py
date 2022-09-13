07be6b57c0df1b6264cbc776bd1ecc68f3d822b9
desertcoqui <fdetres@my365.bellevue.edu>
2022-09-13 00:40:46 -0500

    testing

--- pythonPlayground/whileLoops.py
+++ pythonPlayground/whileLoops.py
@@ -1,6 +1,10 @@
 
-# only prints to #51 ..Line 7 passes the variable to line 4, line 5 evaluates a true statement and prints out 51 on line 6. Line 7 subtracts 51- 1 to give iterations a reassigned value of 50 but line 8 does not print the variable, instead it calls the print() method from line 4.
-iterations = 65
+# only prints to #51 ..Line 7 passes the variable to line 4,
+# line 5 evaluates a true statement and prints out 51 on line 6.
+# Line 7 subtracts 51- 1 to give iterations a reassigned value of 50
+# but line 8 does not print the variable,
+# instead it calls the print() method from line 4.
+iterations = 67
 print(iterations)
 while iterations > 50:
     print(iterations)
@@ -8,8 +12,9 @@ while iterations > 50:
 print()
 
 
-# prints to 50 because the variable is calling to line 16 after it subtracts 1
-iterations_two = 65
+# prints to 50 because the variable is calling to
+# line 16 after it subtracts 1
+iterations_two = 67
 while iterations_two > 50:
     print(iterations_two)
     iterations_two = iterations_two - 1
